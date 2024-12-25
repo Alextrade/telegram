e03ee1d69fa8fd708f2ecc1eb26e9298bb115814
Alextrade <40890651+Alextrade@users.noreply.github.com>
2024-12-25 12:29:46 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+// Экспорт конфигурации для Vite
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+      ],
+      refresh: true, // Обновление при изменении файлов
+    }),
+  ],
+  server: {
+    host: '0.0.0.0', // Нужно для Render, чтобы приложение было доступно снаружи
+    port: 3000, // Порт для запуска приложения (можно настроить по необходимости)
+  },
+  build: {
+    outDir: 'public/build', // Папка, куда будет собираться проект (это важно для Render)
+  },
 });
